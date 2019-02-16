d6fe130f36f4e04920150e2cf774b1074d5a99fa
Sharon Manrique-Jimenez <sharon.manrique2@hotmail.com>
2019-02-15 22:33:11 -0500

    update css

--- src/app.js
+++ src/app.js
@@ -1,40 +1,61 @@
 /* global window */
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import MapGL, { Marker, Popup, NavigationControl } from 'react-map-gl';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import MapGL, { Marker, Popup, NavigationControl } from "react-map-gl";
 
-import ControlPanel from './control-panel';
-import CityPin from './city-pin';
-import CityInfo from './city-info';
+import ControlPanel from "./control-panel";
+import CityPin from "./city-pin";
+import CityInfo from "./city-info";
 
-import CITIES from '../data/cities.json';
+import CITIES from "../data/cities.json";
 
-const TOKEN = 'pk.eyJ1IjoiZm9vcGVydCIsImEiOiJjanM1bDlxbmgwMDUwNGFtZHFxZ3M2NGx1In0.N6t4n3TBDjXSGXeZ2QyosQ'; // Set your mapbox token here
+import Countdown from "react-countdown-now";
+
+const TOKEN =
+  "pk.eyJ1IjoiZm9vcGVydCIsImEiOiJjanM1bDlxbmgwMDUwNGFtZHFxZ3M2NGx1In0.N6t4n3TBDjXSGXeZ2QyosQ"; // Set your mapbox token here
 
 const navStyle = {
-  position: 'absolute',
+  position: "absolute",
   top: 0,
   left: 0,
-  padding: '10px'
+  padding: "10px"
 };
 
 class NavBar extends React.Component {
   render() {
     return (
       <div className="nav-container">
-        <div className="nav-grid-phase">
-          <div className="nav-bar-options" id="phase-one">PHASE 1</div>
-          <div className="nav-bar-options" id="phase-two">PHASE 2</div>
-          <div className="nav-bar-options" id="phase-three"> PHASE 3</div>
+        <div className="nav-grid">
+          <div className="nav-bar-options">PHASE 1</div>
+          <div className="nav-bar-options">
+            <a href="#phase-two">PHASE 2</a>
+          </div>
+          <div className="nav-bar-options">PHASE 3</div>
         </div>
       </div>
-    )
+    );
   }
 }
 
+class PhaseTwo extends React.Component {
+  render() {
+    return (
+      <div className="phase-two-body" id="phase-two">
+        <h2>Time to Stake</h2>
+        <p>
+          Staking ensures that the next ETHGlobal event is held that the city of
+          your choosing. Staking also acts as your payment for a ticket. If your
+          city of choice does not win majority vote, your funds will be released
+          back to you at the end of the staking period.
+        </p>
+        <div className="winner-one" />
+        <div className="winner-two" />
+      </div>
+    );
+  }
+}
 
 export default class Map extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -49,41 +70,47 @@ export default class Map extends Component {
     };
   }
 
-  _updateViewport = (viewport) => {
+  _updateViewport = viewport => {
     this.setState({ viewport });
-  }
+  };
 
   _renderCityMarker = (city, index) => {
     return (
       <Marker
         key={`marker-${index}`}
         longitude={city.longitude}
-        latitude={city.latitude} >
-        <CityPin name={city.city} size={20} onClick={() => this.setState({ popupInfo: city })} />
+        latitude={city.latitude}
+      >
+        <CityPin
+          name={city.city}
+          size={20}
+          onClick={() => this.setState({ popupInfo: city })}
+        />
       </Marker>
     );
-  }
+  };
 
   _renderPopup() {
     const { popupInfo } = this.state;
 
-    return popupInfo && (
-      <Popup tipSize={5}
-
-        anchor="top"
-        longitude={popupInfo.longitude}
-        latitude={popupInfo.latitude}
-        closeOnClick={false}
-
-        onClose={() => this.setState({ popupInfo: null })} >
-        <CityInfo info={popupInfo} />
-        <h1> Test </h1>
-      </Popup>
+    return (
+      popupInfo && (
+        <Popup
+          tipSize={5}
+          anchor="top"
+          longitude={popupInfo.longitude}
+          latitude={popupInfo.latitude}
+          closeOnClick={false}
+          onClose={() => this.setState({ popupInfo: null })}
+        >
+          <CityInfo info={popupInfo} />
+          <h1> Test </h1>
+        </Popup>
+      )
     );
   }
 
   render() {
-
     const { viewport } = this.state;
 
     return (
@@ -93,8 +120,8 @@ export default class Map extends Component {
         height="100%"
         mapStyle="mapbox://styles/mapbox/dark-v9"
         onViewportChange={this._updateViewport}
-        mapboxApiAccessToken={TOKEN} >
-
+        mapboxApiAccessToken={TOKEN}
+      >
         {CITIES.map(this._renderCityMarker)}
 
         {this._renderPopup()}
@@ -103,18 +130,23 @@ export default class Map extends Component {
           <NavigationControl onViewportChange={this._updateViewport} />
         </div>
       </MapGL>
-
-
     );
   }
-
 }
 
 function App() {
-  return <div className="entire-app">
-    <NavBar />
-    <div className="phase-one"><Map /></div>
-  </div>
+  return (
+    <div className="entire-app">
+      <NavBar />
+      <div className="phase-one">
+        <div className="count-down">
+          <Countdown date={Date.now() + 2.592e+9} />
+        </div>
+        <Map />
+      </div>
+      <PhaseTwo />
+    </div>
+  );
 }
 
 export function renderToDom(container) {
