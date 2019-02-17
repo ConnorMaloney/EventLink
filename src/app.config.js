178211df05604ec035f943481d06741d6806d5e2
henrynguyen5 <ngu.henry@hotmail.com>
2019-02-17 06:07:57 -0500

    Refactor

---
+++ src/app.config.js
@@ -0,0 +1,4 @@
+export const appConfig = {
+  mapBoxToken:
+    "pk.eyJ1IjoiZm9vcGVydCIsImEiOiJjanM1bDlxbmgwMDUwNGFtZHFxZ3M2NGx1In0.N6t4n3TBDjXSGXeZ2QyosQ"
+};
---
+++ src/repositories/tweets.repo.js
@@ -0,0 +1,3 @@
+export function getTweetsForCity(tweets, city) {
+  return tweets[city.toLowerCase()];
+}
